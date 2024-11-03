0378bc401a07a808fad61464a2ca62c7448bc920
dani-sie <dani-sie@uniandes.edu.co>
2024-11-03 18:31:47 -0500

    se crean los modulos, los servicios, la estructura de recursos y se implementa el listado de recursos

---
+++ cafe-daniel-sierra/src/app/cafe/cafe-list/cafe-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
+import { dataCafes } from './dataCafes';
+import cafeJson from './cafes.json';
+
+@Component({
+  selector: 'app-cafe-list',
+  templateUrl: './cafe-list.component.html',
+  styleUrls: ['./cafe-list.component.css']
+})
+export class CafeListComponent implements OnInit {
+
+  /*cafes: Array<Cafe> = [];
+
+  constructor() { }
+
+  getCafeList(): Array<Cafe> {
+    return dataCafes;
+  }
+
+  ngOnInit() {
+    this.cafes = this.getCafeList();
+  }*/
+    cafes: Array<Cafe> = [];
+
+    constructor(private cafeService: CafeService) { }
+   
+    getCafes(): void {
+      this.cafes = cafeJson;
+      /*this.cafeService.getCafes().subscribe((cafes) => {
+        this.cafes = cafes;
+      });*/
+    }
+   
+    ngOnInit() {
+      this.getCafes();
+    }
+
+}
---
+++ cafe-daniel-sierra/src/app/cafe/cafe-list/dataCafes.ts
@@ -0,0 +1,6 @@
+import { Cafe } from "../cafe";
+
+export const dataCafes = [
+ new Cafe(1,"Café Especial para tí", "Blend", "Angelópolis, Antioquia","Panela, Durazno, Caramelo",1920,"https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especial-para-ti-colores-cafe-colombiano-f_1_720x.jpg"),
+
+];
