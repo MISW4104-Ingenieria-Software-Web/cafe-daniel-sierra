87f1f83b8edc7890de2fb3219bd151e609cf005b
dani-sie <dani-sie@uniandes.edu.co>
2024-11-03 18:57:30 -0500

    se instala la libreria de faker y se ejecutan las pruebas

--- cafe-daniel-sierra/src/app/cafe/cafe.service.spec.ts
+++ cafe-daniel-sierra/src/app/cafe/cafe.service.spec.ts
@@ -1,16 +1,51 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
+import { HttpClientModule } from '@angular/common/http';
+import { CafeListComponent } from './cafe-list/cafe-list.component';
 import { CafeService } from './cafe.service';
+import { Cafe } from './cafe';
 
-describe('Service: Cafe', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CafeService]
-    });
-  });
+describe('CafeListComponent', () => {
+ let component: CafeListComponent;
+ let fixture: ComponentFixture<CafeListComponent>;
+ let debug: DebugElement;
 
-  it('should ...', inject([CafeService], (service: CafeService) => {
-    expect(service).toBeTruthy();
-  }));
+ beforeEach(async(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientModule],
+     declarations: [ CafeListComponent ],
+     providers: [ CafeService ]
+   })
+   .compileComponents();
+ }));
+
+
+ beforeEach(() => {
+   fixture = TestBed.createComponent(CafeListComponent);
+   component = fixture.componentInstance;
+   component.cafes = [
+     new Cafe(faker.number.int(), faker.person.fullName(), faker.person.fullName(), faker.person.fullName(), faker.person.fullName(),faker.number.int(), faker.person.fullName())
+   ]
+
+   fixture.detectChanges();
+   debug = fixture.debugElement;
+ });
+
+ it('should create', () => {
+   expect(component).toBeTruthy();
+ });
+
+ it("Component has a table", () => {
+   expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
+ });
+
+ it('should have an dd element ', () => {
+   const dd = debug.query(By.css('dd'));
+   const content: HTMLElement = dd.nativeElement;
+   expect(content.textContent).toEqual(component.cafes[0].nombre)
+ });
+
+
 });
